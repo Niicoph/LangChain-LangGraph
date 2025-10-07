f70de8bc87baf32f797c1bfa90bc753852c64bf7
Niicoph <niicopsc@gmail.com>
2025-10-07 12:06:24 -0300

    update search agent

--- main.py
+++ main.py
@@ -1,31 +1,20 @@
+from langchain.agents import create_react_agent
+from langchain.agents import AgentExecutor
+from langchain import hub
 from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from langchain_tavily import TavilySearch  # Tavily Search tool
 
 load_dotenv()
 
-
-from langchain import hub
-from langchain.agents import AgentExecutor
-from langchain.agents.react.agent import create_react_agent
-from langchain_openai import ChatOpenAI
-from langchain_tavily import TavilySearch
-
-# ReAct agent -> looping controller that simulates reasoning
-# Thought: "I should search LinkedIn."
-# Action: TavilySearch("AI jobs")
-# Observation: (search results)
-# Thought: "Now I should summarize."
-# Action: "Write final answer.
-
+# A create_react_agent is a function that creates a react agent. This takes a few parameters such as: LLM, tools, prompt, output_parser, etc.
+# The AgentExecutor is the runtime. It is responsible for executing the agent. Actually runs the tools when the agent decides to use them.
 
 tools = [TavilySearch()]
+llm = ChatOpenAI(model="gpt-4o-mini")
 
-# Since ChatOpenAI automatically injects a "stop" tokens parameter when building requests for agents, models that don't support stop sequences won't work
-# OpenAI's API rejects the request -> hence 400 BadRequestError.
-llm = ChatOpenAI(model="gpt-4o", temperature=0)
-# Why the stop? because it tells the agent to stop after a string is matched. Then, Agent can (thought -> action -> input) and decide the next move.
-
+# Receives tools , tools_names and input question by the user
 react_prompt = hub.pull("hwchase17/react")
-
 
 agent = create_react_agent(
     llm=llm,
@@ -33,30 +22,17 @@ agent = create_react_agent(
     prompt=react_prompt,
 )
 
-
-agent_executor = AgentExecutor(
-    agent=agent,
-    tools=tools,
-    verbose=True,
-    handle_parsing_errors=True,
-    max_iterations=3
-)
-
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
 
 chain = agent_executor
 
 
 def main():
-
     result = chain.invoke(
-        input={
-            "input": "search for 3 job postings for an ai engineer using langchain in europe on linkedin and list their details",
-        }
-    )
+        {"input": "Search for 3 job postings for an AI engineer in Europe, particularly in Spain. List their details."})
 
     print(result)
 
 
 if __name__ == "__main__":
-
     main()
